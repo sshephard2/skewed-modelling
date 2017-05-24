d53f1d98190541e82c33ef80b9e72b0b3c8552aa
Stephen John Shephard <s.shephard2@ncl.ac.uk>
2017-05-24 10:08:36 +0100

    Spring Boot Maven project structure

---
+++ Code/simplemicro/src/main/java/uk/ncl/ac/sshephard/simplemicro/SimplemicroApplication.java
@@ -0,0 +1,12 @@
+package uk.ncl.ac.sshephard.simplemicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplemicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplemicroApplication.class, args);
+	}
+}
