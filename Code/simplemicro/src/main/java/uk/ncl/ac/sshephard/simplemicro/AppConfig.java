28e8d7da022cc7560996fe85e034d188566dd797
Stephen John Shephard <s.shephard2@ncl.ac.uk>
2017-05-26 16:19:01 +0100

    Externalise Cassandra config to cassandra.properties file
    Also, extend AbstractCassandraConfiguration to return the config to the
    Spring context

--- Code/simplemicro/src/main/java/uk/ncl/ac/sshephard/simplemicro/AppConfig.java
+++ Code/simplemicro/src/main/java/uk/ncl/ac/sshephard/simplemicro/AppConfig.java
@@ -1,19 +1,36 @@
 package uk.ncl.ac.sshephard.simplemicro;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.Session;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.cassandra.config.java.AbstractCassandraConfiguration;
 
 @Configuration
-public class AppConfig {
+@PropertySource(value = { "classpath:cassandra.properties" })
+public class AppConfig extends AbstractCassandraConfiguration {
 
-  /*
-   * Use the standard Cassandra driver API to create a com.datastax.driver.core.Session instance.
-   */
-  public @Bean Session session() {
-    Cluster cluster = Cluster.builder().addContactPoints("52.50.71.127").build();
-    return cluster.connect("Simplemicro");
-  }
+	@Autowired
+	private Environment env;
+	
+	/*
+	 * Provide a contact point to the configuration.
+	 */
+	public String getContactPoints() {
+		return env.getProperty("cassandra.contactpoints");
+	}
+
+	/*
+	 * Provide a port to the configuration.
+	 */
+	public int getPort() {
+		return Integer.parseInt(env.getProperty("cassandra.port").trim());
+	}
+	
+	/*
+	 * Provide a keyspace name to the configuration.
+	 */
+	public String getKeyspaceName() {
+		return env.getProperty("cassandra.keyspace");
+	}
 }
