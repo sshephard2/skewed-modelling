29ac387fb60f9dfb91f36c7db37129946dd60d62
Stephen John Shephard <s.shephard2@ncl.ac.uk>
2017-06-19 12:01:55 +0100

    Update simple AngularJS code to display all returned tickets

--- Code/simplemicro/public/ticket.js
+++ Code/simplemicro/public/ticket.js
@@ -2,6 +2,6 @@ angular.module('demo', [])
 .controller('Ticket', function($scope, $http) {
     $http.get('http://localhost:8080/search').
         then(function(response) {
-            $scope.booking = response.data;
+            $scope.tickets = response.data;
         });
 });
