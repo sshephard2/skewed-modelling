3ecf822a1808a836c079ab37d3ba3a1678acd4e9
Stephen John Shephard <s.shephard2@ncl.ac.uk>
2017-06-26 08:44:33 +0100

    Update key annotations and make Ticket final
    Also add Javadocs comments

--- Code/simplemicro/src/main/java/uk/ncl/ac/sshephard/simplemicro/Ticket.java
+++ Code/simplemicro/src/main/java/uk/ncl/ac/sshephard/simplemicro/Ticket.java
@@ -4,17 +4,31 @@ import org.springframework.cassandra.core.PrimaryKeyType;
 import org.springframework.data.cassandra.mapping.PrimaryKeyColumn;
 import org.springframework.data.cassandra.mapping.Table;
 
+/**
+ * Ticket entity
+ * @author Stephen Shephard
+ */
 @Table
-public class Ticket {
-	
-	@PrimaryKeyColumn(name = "id", ordinal = 1, type = PrimaryKeyType.CLUSTERED)
-	private final int id; // unique ticket id
+public final class Ticket {
+
 	@PrimaryKeyColumn(name = "sport", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
 	private final String sport; // type of sport
-	private final int day; // day of event
-	private final int seat; // seat number
+	@PrimaryKeyColumn(name = "owner", ordinal = 1, type = PrimaryKeyType.CLUSTERED)
 	private final String owner; // name of ticket owner (for booked ticket)
+	@PrimaryKeyColumn(name = "day", ordinal = 2, type = PrimaryKeyType.CLUSTERED)
+	private final int day; // day of event
+	@PrimaryKeyColumn(name = "id", ordinal = 3, type = PrimaryKeyType.CLUSTERED)
+	private final int id; // unique ticket id
+	private final int seat; // seat number
 
+	/**
+	 * Constructor for ticket POJO
+	 * @param id - unique ticket id
+	 * @param sport - type of sport (Athletics, Cycling, Diving)
+	 * @param day - day of event (day 1 to N)
+	 * @param seat - seat number
+	 * @param owner - name of ticket owner (for booked tickets)
+	 */
 	public Ticket(int id, String sport, int day, int seat, String owner) {
 		this.id = id;
 		this.sport = sport;
