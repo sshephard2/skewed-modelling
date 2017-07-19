17e09e5f70576d8a26eaf730aea0fe498e3aa7a6
Stephen John Shephard <s.shephard2@ncl.ac.uk>
2017-07-19 16:30:17 +0100

    DB with replication test run 2 results

--- Code/tools/procount.py
+++ Code/tools/procount.py
@@ -6,10 +6,10 @@
 import csv
 
 # Metrics filename
-METRICS_FILE = '../dbreplication/results/run1/db2/org.apache.cassandra.metrics.ThreadPools.CompletedTasks.request.ReadStage.csv'
+METRICS_FILE = '../dbreplication/results/run2/db3/org.apache.cassandra.metrics.ThreadPools.CompletedTasks.request.ReadStage.csv'
 
 # Processed output filename
-OUTPUT_FILE = '../dbreplication/results/run1/pro_db2.csv'
+OUTPUT_FILE = '../dbreplication/results/run2/pro_db3.csv'
 
 # Starting values
 start_timestamp = 0
